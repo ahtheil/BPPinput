198b794c480c29cebc67025233219ce608888ac0
atheil <atheil1282@sdsu.edu>
2021-11-18 09:54:19 -0800

    The second test commit

--- script.py
+++ script.py
@@ -16,4 +16,22 @@ directory = r'/Users/andytheil/Desktop/Hypochiluswork/BPP run for concatenated/c
 onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
 
 
+outF = open("/Users/andytheil/Desktop/Hypochiluswork/BPP run for concatenated/pythontestoutput.txt","w")
 
+
+#Loop through directory to get data from each file, store in python, 
+for file in onlyfiles:
+    f = open(("/Users/andytheil/Desktop/Hypochiluswork/BPP run for concatenated/converttophylip/"+file),"rb")
+    contents = f.read()
+    
+    #Next step, write a if statement to check if individuals are on list
+    
+    outF.write(str(contents))
+    
+    #Write to a output file
+    outF.write("\n")
+    f.close()
+    
+    
+#Close output file
+outF.close()
