0b032869ee2bbf4e2a9263d3ef8ef4595687586b
atheil <atheil1282@sdsu.edu>
2021-11-21 15:23:03 -0800

    Commit before I make changes in trying to parse taxa

--- script.py
+++ script.py
@@ -1,35 +1,44 @@
-#This is the script to begin the project with
+#This script will take a directory of phylip alingment files
+#It will read them and only include specified taxa in the output
+#
+#Written by Addy Theil
+#Last change 11/21/2021
 
-#The goal of this script is to read alignment phylip files in a directory
-#The output will be a single fuile of phylip files with all alignments included 
-#in the file. The utput wil be in specifically BPP format. Additionaly, we will 
-#Filter based on species we are including in analysis
 
-
-import os
+#Import functions from package os to read files from directory
 from os import listdir
 from os.path import isfile, join
 
-
-
-directory = r'/Users/andytheil/Desktop/Hypochiluswork/BPP run for concatenated/converttophylip/'
+#Make the list of file names
+directory = '/Users/andytheil/Desktop/Hypochiluswork/File_formating/phylipalignment550/'
 onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
 
+#Make a list for tags of sequences we want to keep in analysis, excluding taxa we dont want
 
-outF = open("/Users/andytheil/Desktop/Hypochiluswork/BPP run for concatenated/pythontestoutput.txt","w")
+includetaxa = ["CA_H_bernardino_G2894","CA_H_bernardino_G2898","CA_H_bernardino_H025",
+        "CA_H_petrunkevitchi_ALTA_G2601","CA_H_petrunkevitchi_TULE_G2463",
+        "CA_H_petrunkevitchi_TULE_G2476","CA_H_petrunkevitchi_KING_G2543",
+        "CA_H_petrunkevitchi_SAN_G2509","CA_H_petrunkevitchi_SEQ_G2251",
+        "CA_H_petrunkevitchi_SEQ_G2488","CA_H_petrunkevitchi_KING_G2550",
+        "CA_H_petrunkevitchi_YOSE_G2561","CA_H_petrunkevitchi_YOSE_G2566",
+        "CA_H_petrunkevitchi_SAN_G2558","CA_H_kastoni_G2519","CA_H_kastoni_NEY_H324"
+        ]
+
+#Open an output file to write results to
+outF = open("/Users/andytheil/Desktop/Hypochiluswork/File_formating/pythontestoutput.txt","w")
 
 
 #Loop through directory to get data from each file, store in python, 
 for file in onlyfiles:
-    f = open(("/Users/andytheil/Desktop/Hypochiluswork/BPP run for concatenated/converttophylip/"+file),"rb")
+    f = open(("/Users/andytheil/Desktop/Hypochiluswork/File_formating/phylipalignment550/"+file),"r")
     contents = f.read()
-    
+
     #Next step, write a if statement to check if individuals are on list
     
     outF.write(str(contents))
     
     #Write to a output file
-    outF.write("\n")
+
     f.close()
     
     
