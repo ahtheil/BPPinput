fc04dda723c67feaad59ceecec72f63b46e072b1
atheil <atheil1282@sdsu.edu>
2021-11-21 16:02:52 -0800

    Final edits for sunday. Next step is changing header to reflect taxa included

--- script.py
+++ script.py
@@ -13,17 +13,6 @@ from os.path import isfile, join
 directory = '/Users/andytheil/Desktop/Hypochiluswork/File_formating/phylipalignment550/'
 onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
 
-#Make a list for tags of sequences we want to keep in analysis, excluding taxa we dont want
-
-includetaxa = ["CA_H_bernardino_G2894","CA_H_bernardino_G2898","CA_H_bernardino_H025",
-        "CA_H_petrunkevitchi_ALTA_G2601","CA_H_petrunkevitchi_TULE_G2463",
-        "CA_H_petrunkevitchi_TULE_G2476","CA_H_petrunkevitchi_KING_G2543",
-        "CA_H_petrunkevitchi_SAN_G2509","CA_H_petrunkevitchi_SEQ_G2251",
-        "CA_H_petrunkevitchi_SEQ_G2488","CA_H_petrunkevitchi_KING_G2550",
-        "CA_H_petrunkevitchi_YOSE_G2561","CA_H_petrunkevitchi_YOSE_G2566",
-        "CA_H_petrunkevitchi_SAN_G2558","CA_H_kastoni_G2519","CA_H_kastoni_NEY_H324"
-        ]
-
 #Open an output file to write results to
 outF = open("/Users/andytheil/Desktop/Hypochiluswork/File_formating/pythontestoutput.txt","w")
 
@@ -31,16 +20,26 @@ outF = open("/Users/andytheil/Desktop/Hypochiluswork/File_formating/pythontestou
 #Loop through directory to get data from each file, store in python, 
 for file in onlyfiles:
     f = open(("/Users/andytheil/Desktop/Hypochiluswork/File_formating/phylipalignment550/"+file),"r")
-    contents = f.read()
-
-    #Next step, write a if statement to check if individuals are on list
     
-    outF.write(str(contents))
+#loop through each sequence in the file and check the header as CA  
+    taxa = f.readlines()
+    header = taxa[0]
     
-    #Write to a output file
-
+    
+    
+    for seq in taxa:
+        
+        counter = 0
+        if (seq[0:2] == "CA"):
+            counter += 1
+            outF.write(str(seq))
+        #where I need to go from here is to complete the header
+        #In order to specify the header correctly, I need to count the
+        #number of files included and change the header to reflect that
+        
+        
+        
     f.close()
-    
-    
 #Close output file
+
 outF.close()
